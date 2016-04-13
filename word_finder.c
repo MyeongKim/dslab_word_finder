0e2abb49945b13f4f9ac85147e022f18c12dcc67
mingkim <ming3772@gmail.com>
2016-04-13 13:09:45 +0900

    repeat 5 times

--- word_finder.c
+++ word_finder.c
@@ -84,51 +84,52 @@ int main(void)
 	char token_array[1000][1000];
 	int match_count=0;
 	int match_count_sum=0;
-	int i;
+	int i,j;
 
-	ptr_file = fopen("sample.txt", "r");
-	if (!ptr_file)
-		return 1;
+	for(i=0; i<5;i++){
+		ptr_file = fopen("sample.txt", "r");
+		if (!ptr_file)
+			return 1;
 
-	printf("단어를 입력하세요 : ");
-	scanf("%s", input);
+		printf("단어를 입력하세요 : ");
+		scanf("%s", input);
+		match_count_sum=0;	
+		// Get one line and save to buffer
+		 while(fgets(buf, 1000, ptr_file) != NULL){
+			token = strtok(buf, seps);
+			match_count = 0;
+			while(token != NULL){
+				match_result = 0;
+				match_string(input, token, &match_result);
+				if(match_result == 1){
+		 			if(line_start == 0){
+						printf("\nline#%d: ", line_num);
+						line_start = 1;
+					}
+					strcpy(token_array[match_count], token);
+					match_count = match_count + 1;
+				}
+				token = strtok(NULL, seps);
+			}
+			
+			if(match_count != 0){
+				// print matched words from token_array
+				for(j=0; j< match_count; j++){
+					printf("%s", token_array[j]);
+					if ( j != match_count-1){
+						printf(", ");
+					}
+				}
+			}
 	
-	// Get one line and save to buffer
-	 while(fgets(buf, 1000, ptr_file) != NULL){
-		token = strtok(buf, seps);
-		match_count = 0;
-		while(token != NULL){
-			match_result = 0;
-			match_string(input, token, &match_result);
-			if(match_result == 1){
-	 			if(line_start == 0){
-					printf("\nline#%d: ", line_num);
-					line_start = 1;
-				}
-				strcpy(token_array[match_count], token);
-				match_count = match_count + 1;
-			}
-			token = strtok(NULL, seps);
+			match_count_sum = match_count_sum + match_count;
+			line_start = 0;
+			line_num = line_num + 1;
 		}
+		printf("\n\n총 %d 개의 %s 를 찾았습니다. \n", match_count_sum, input);
 		
-		if(match_count != 0){
-			// print matched words from token_array
-			for(i=0; i< match_count; i++){
-				printf("%s", token_array[i]);
-				if ( i != match_count-1){
-					printf(", ");
-				}
-			}
-		}
-
-		match_count_sum = match_count_sum + match_count;
-
-		
-		line_start = 0;
-		line_num = line_num + 1;
-	}
-	printf("\n\n총 %d 개의 %s 를 찾았습니다. \n", match_count_sum, input);
-	printf("\n");
-	fclose(ptr_file);
+		printf("\n");
+		fclose(ptr_file);
+	}	
 	return 0;
 }
